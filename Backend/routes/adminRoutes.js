0e26e4b2689c66a9a3a1c2f340382362c90671fd
Susheel <susheelbaral@gmail.com>
2025-09-13 23:02:34 +0545

    Integrated the stripe payment option.

--- Backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { adminLogin } = require("../controllers/authController");
-
-const router = express.Router();
-
-// POST /admin/login
-router.post("/login", adminLogin);
-
-module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,62 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-
-const adminRoutes = require("./routes/adminRoutes");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
+
+const path = require("path");
+app.use(express.json());
+
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 
-app.use("/admin", adminRoutes);
+
 
 // Serve static files from the root directory
-const path = require("path");
-app.use(express.static(path.join(__dirname, "../")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
+  console.log(
+    "Serving index.html from:",
+    path.join(__dirname, "../index.html")
+  );
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-// ========= Server =========
+app.use(express.static(path.join(__dirname, "../")));
+
+app.post("/checkout", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Prominent Product",
+            },
+            unit_amount: 50 * 100, // Amount in cents
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      shipping_address_collection: {
+        allowed_countries: ['US'],
+      },
+      success_url: `${process.env.BASE_URL}`,
+      cancel_url: `${process.env.BASE_URL}`,
+    });
+
+    // 👉 Send session info to frontend
+    res.json({ url: session.url });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
   console.log(`🚀 Server running on http://localhost:${port}`)
